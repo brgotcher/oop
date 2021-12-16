4d729b45a52311559e6629deb080c619b171d0fd
brgotcher <brgotcher@gmail.com>
2021-12-16 06:06:54 -0600

    get cast list for two movies and find common cast

--- main.py
+++ main.py
@@ -18,20 +18,61 @@ class Movie:
         self.title = title
         self.cast = []
 
+title = input("Enter a movie title: ")
 
-details = requests.get("https://api.themoviedb.org/3/search/movie?api_key=" + KEY + "&query=Jack+Reacher")
+title = title.replace(" ", "+")
+
+movie1 = Movie(title)
+title = input("Enter a second movie title: ")
+title = title.replace(" ", "+")
+movie2 = Movie(title)
+
+# =============== get cast for movie1
+details = requests.get("https://api.themoviedb.org/3/search/movie?api_key=" + KEY + "&query=" + movie1.title)
 details = details.json()
 id = str(details["results"][0]["id"])
-print("movie id: " + id)
+# print("movie id: " + id)
 cast = requests.get("https://api.themoviedb.org/3/movie/" + id + "/credits?api_key=" + KEY + "&language=en-US")
 cast = cast.json()
-print(cast)
+# print(cast)
 
 cast = cast["cast"]
-print(cast)
+# print(cast)
 
 castList = []
 for actor in cast:
     castList.append(actor["name"])
-print(castList)
+movie1.cast = castList
 
+
+
+# ======================= get cast for movie2 =====================================
+
+details = requests.get("https://api.themoviedb.org/3/search/movie?api_key=" + KEY + "&query=" + movie2.title)
+details = details.json()
+id = str(details["results"][0]["id"])
+# print("movie id: " + id)
+cast = requests.get("https://api.themoviedb.org/3/movie/" + id + "/credits?api_key=" + KEY + "&language=en-US")
+cast = cast.json()
+# print(cast)
+
+cast = cast["cast"]
+# print(cast)
+
+castList = []
+for actor in cast:
+    castList.append(actor["name"])
+movie2.cast = castList
+
+print(movie1.cast)
+print()
+print(movie2.cast)
+
+common = []
+
+for actor in movie1.cast:
+    if actor in movie2.cast:
+        common.append(Actor(actor))
+print("Common: ")
+for actor in common:
+    print(actor.name)
